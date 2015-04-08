d222996f175fc15a2fb91ca8c9217457780b1c1e
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-04-08 15:46:45 -0700

    Create glycanbuild.R

---
+++ glycanbuild.R
@@ -0,0 +1,16 @@
+#Script for building glycans. Ideally, the option for building glycans based on IUPAC polysaccharide nomenclature would be useful, but 
+#perhaps it would be easier to build glycan geometry files based on a matrix input with SMILES string modifications (sulfate, phosphate, 
+#N-acetyl, etc...)
+#The library represents a series of geometrically defined monosaccharides which the function will draw from in order to build the glycan 
+#structure.
+#
+#
+#
+glycanbuild<-function(glycanstring){
+     
+     glycanstring<-as.character(glycanstring)
+
+library<-
+     
+     
+}
