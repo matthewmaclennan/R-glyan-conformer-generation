1e7e02d7bbc92ab52c2197097f1de129b6042bf7
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-16 12:17:03 -0700

    Update glycanbuild.R

--- glycanbuild.R
+++ glycanbuild.R
@@ -4,13 +4,18 @@
 #The library represents a series of geometrically defined monosaccharides which the function will draw from in order to build the glycan 
 #structure.
 #
-#
-#
+#STEP 1: Number each monomer in the glycan name string. Turn the glycan name into a vector of monomers.
+########: Regex letters with noncapturing positive lookahead for alpha/beta/a/b/A/B characters
+as.matrix(regmatches(glycan,gregexpr("[A-Za-z]+(?=[aAbB(alpha)(beta)])",glycan,perl=T))[[1]])
+#analogous regex as above, but for a numbered linkage from monomer to amino acid or molecule "R".
+as.matrix(regmatches(glycan,gregexpr("[A-Za-z]+(?=[aAbB(alpha)(beta)][0-9]-R)",glycan,perl=T))[[1]])
+#STEP 2: regex out the connectors (alpha, beta, 1-4, etc...). Further split these into bond type (letter), first monomer position and 
+#second monomer position.
 glycanbuild<-function(glycanstring){
      
      glycanstring<-as.character(glycanstring)
 
-     strsplit(glycanstring,"\\([abAB]-[0-9]")
+     strsplit(glycanstring,"[abAB][0-9]-[0-9]")
      
      
 }
