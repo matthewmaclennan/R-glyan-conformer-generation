851ec98900e231df4c038cbe011c263fb837f9d7
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-16 19:10:24 -0700

    Update KEGGRestGlycan.R

--- KEGGRestGlycan.R
+++ KEGGRestGlycan.R
@@ -2,6 +2,13 @@
 glycanmatch<-regmatches(getURL("http://rest.kegg.jp/get/gl:G00003"),
 gregexpr("NODE.*EDGE.*///",getURL("http://rest.kegg.jp/get/gl:G00003")))[[1]]
 #remove "\n" and display neatly
-as.matrix(strsplit(glycanmatch,"\n")[[1]])
+match<-as.matrix(strsplit(glycanmatch,"\n")[[1]])
 #Find "NODE" and "EDGE" labels
-grep("NODE|EDGE",as.matrix(strsplit(glycanmatch,"\n")[[1]]))
+n<-grep("NODE",strsplit(glycanmatch,"\n")[[1]])
+e<-grep("EDGE",strsplit(glycanmatch,"\n")[[1]])
+#NODES matrix
+NODES<-as.matrix(ldply(strsplit(strsplit(glycanmatch,"\n")[[1]][(n+1):(e-1)]," +"),
+function(x) x[-1]))[,-1]
+#EDGES matrix
+EDGES<-as.matrix(ldply(strsplit(strsplit(glycanmatch,"\n")[[1]][(e+1):(length(strsplit(glycanmatch,"\n")[[1]])-1)]," +"),
+function(x) x[-1]))[,-1]
