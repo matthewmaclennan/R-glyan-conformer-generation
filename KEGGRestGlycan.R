5380f795a53b3597a999a91b346791dc6142645f
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-16 21:51:19 -0700

    Update KEGGRestGlycan.R

--- KEGGRestGlycan.R
+++ KEGGRestGlycan.R
@@ -13,11 +13,14 @@ NODES<-as.matrix(ldply(strsplit(strsplit(glycanmatch,"\n")[[1]][(n+1):(e-1)]," +
 EDGES<-as.matrix(ldply(strsplit(strsplit(glycanmatch,"\n")[[1]][(e+1):(length(strsplit(glycanmatch,"\n")[[1]])-1)]," +"),
   function(x) x[-1]))[,-1]
 #actual nodes connectivity list in order (edgelist ready for igraph)
-edgelist<-matrix(as.numeric(unlist(apply(EDGES,1,function(x) strsplit(x,":[AaBb][0-9]{,2}|:[0-9]+")))),ncol=2,byrow=T)
+edgenodelist<-matrix(as.numeric(unlist(apply(EDGES,1,function(x) strsplit(x,":[AaBb][0-9]{,2}|:[0-9]+")))),ncol=2,byrow=T)
 #actual edge labels (alpha/beta and conectivity)
-edgelabels<-as.matrix(ldply(strsplit(apply(EDGES,1,function(x) paste0(x,collapse="-")),"[0-9]+:"),
-  function(x) paste0(x,collapse="")))
-plot(graph.edgelist(edgelist))
-#position string
-posstring<-paste0(unlist(lapply(strsplit(strsplit(glycanmatch,"\n")[[1]][(e+1):(length(strsplit(glycanmatch,"\n")[[1]])-1)]," +"),
-function(x) x[-c(1,2)])),collapse="")
+edgelabels<-as.matrix(ldply(strsplit(apply(EDGES,1,function(x) paste0(x,collapse="-")),"(?<=-)[0-9]|[0-9]*:",perl=T),
+function(x) paste0(x,collapse="")))
+#Create igraph object
+edgelist.ig<-graph.edgelist(edgelist)
+#Insert labels into edgelist
+E(edgelist.ig)$labels<-edgelabels
+V(edgelist.ig)$labels<-NODES[,1]
+plot(edgelist.ig,edge.label=E(edgelist.ig)$labels,vertex.label=V(edgelist.ig)$labels)
+#plot does not have angles programmed into it yet.
