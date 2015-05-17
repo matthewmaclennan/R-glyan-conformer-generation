66026e0c363350f5aeb4cea58bae7bc89c253414
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-16 21:17:22 -0700

    Update KEGGRestGlycan.R

--- KEGGRestGlycan.R
+++ KEGGRestGlycan.R
@@ -8,10 +8,16 @@ n<-grep("NODE",strsplit(glycanmatch,"\n")[[1]])
 e<-grep("EDGE",strsplit(glycanmatch,"\n")[[1]])
 #NODES matrix
 NODES<-as.matrix(ldply(strsplit(strsplit(glycanmatch,"\n")[[1]][(n+1):(e-1)]," +"),
-function(x) x[-1]))[,-1]
+  function(x) x[-1]))[,-1]
 #EDGES matrix
 EDGES<-as.matrix(ldply(strsplit(strsplit(glycanmatch,"\n")[[1]][(e+1):(length(strsplit(glycanmatch,"\n")[[1]])-1)]," +"),
-function(x) x[-1]))[,-1]
+  function(x) x[-1]))[,-1]
+#actual nodes connectivity list in order (edgelist ready for igraph)
+edgelist<-matrix(as.numeric(unlist(apply(EDGES,1,function(x) strsplit(x,":[AaBb][0-9]{,2}|:[0-9]+")))),ncol=2,byrow=T)
+#actual edge labels (alpha/beta and conectivity)
+edgelabels<-as.matrix(ldply(strsplit(apply(EDGES,1,function(x) paste0(x,collapse="-")),"[0-9]+:"),
+  function(x) paste0(x,collapse="")))
+
 #position string
 posstring<-paste0(unlist(lapply(strsplit(strsplit(glycanmatch,"\n")[[1]][(e+1):(length(strsplit(glycanmatch,"\n")[[1]])-1)]," +"),
 function(x) x[-c(1,2)])),collapse="")
